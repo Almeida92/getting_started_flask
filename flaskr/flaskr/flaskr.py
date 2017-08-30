1d3e5ace29583ee4d72a91ddac9e3d9523c51dbd
Felipe Almeida <felipe.almeida@longevo.com.br>
2017-08-30 16:41:15 -0300

    Creating The Database

--- flaskr/flaskr/flaskr.py
+++ flaskr/flaskr/flaskr.py
@@ -18,3 +18,29 @@ def connect_db():
     rv = sqlite3.connect(app.config['DATABASE'])
     rv.row_factory = sqlite3.Row
     return rv
+
+def init_db():
+    db = get_db()
+    with app.open_resource('schema.sql', mode='r') as f:
+        db.cursor().executescript(f.read())
+    db.commit
+
+@app.cli.command('create:schema')
+def createschema_command():
+    """ Initializes the database."""
+    init_db()
+    print("successfully created schema.")
+
+def get_db():
+    """Opens a new database connection if there is none yet for the
+    current application context.
+    """
+    if not hasattr(g, 'sqlite_db'):
+        g.sqlite_db = connect_db()
+    return g.sqlite_db
+
+@app.teardown_appcontext
+def close_db(error):
+    """Closes the database again at the end of the request."""
+    if hasattr(g, 'sqlite_db'):
+        g.sqlite_db.close()
