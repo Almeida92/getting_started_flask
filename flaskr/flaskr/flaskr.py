30db849df833627fdee89f9d3196ad1ef47dc105
Felipe Almeida <felipe.almeida@longevo.com.br>
2017-08-30 17:20:49 -0300

    ajusta nas funcões

--- flaskr/flaskr/flaskr.py
+++ flaskr/flaskr/flaskr.py
@@ -47,7 +47,7 @@ def close_db(error):
 
 @app.route('/')
 def show_entries():
-    db = get_db
+    db = get_db()
     cur = db.execute('select title, text from entries order by id desc')
     entries = cur.fetchall()
     return render_template('show_entries.html', entries=entries)
@@ -56,7 +56,7 @@ def show_entries():
 def add_entry():
     if not  session.get('logged_in'):
         abort(401)
-    db = get_db
+    db = get_db()
     db.execute('insert into entries (title, text) values (?, ?)', [request.form['title'], request.form['text']])
     db.commit
     flash('New entry was successfully posted')
@@ -65,7 +65,7 @@ def add_entry():
 @app.route('/login')
 def login():
     error = None
-    if request.method = 'POST':
+    if request.method == 'POST':
         if request.form['username'] != app.config['USERNAME']:
             error = 'invalid username'
         elif request.form['password'] != app.config['PASSWORD']:
